d0ec369a27219a6933cc5e4523cc27efe6db3509
oseun <okseongeun4@gmail.com>
2021-10-14 14:36:53 +0900

    Updates

---
+++ precache-manifest.c545c0c32e6ceb7e23e978b011d12f8c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9946cf51e29f16d87a282b82475fa87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5efc86b07324b6413037",
+    "url": "/static/css/main.c06db1a9.chunk.css"
+  },
+  {
+    "revision": "b86f4c46e90411c54b13",
+    "url": "/static/js/2.f090c731.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.f090c731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbcc84f6e83af90d35b4",
+    "url": "/static/js/3.15e9b9ab.chunk.js"
+  },
+  {
+    "revision": "5efc86b07324b6413037",
+    "url": "/static/js/main.6cf528ad.chunk.js"
+  },
+  {
+    "revision": "6182cf380e1d1856ff9e",
+    "url": "/static/js/runtime-main.6696ee5f.js"
+  },
+  {
+    "revision": "0316627cd824392e0a79b58ad81b6f36",
+    "url": "/static/media/NotoSansCJKkr-Regular.0316627c.eot"
+  },
+  {
+    "revision": "81f8ae367221a20bc0b6c35477bd205f",
+    "url": "/static/media/NotoSansCJKkr-Regular.81f8ae36.woff"
+  },
+  {
+    "revision": "30069fd6dacc659f3d1e2f9df8f4e823",
+    "url": "/static/media/bg-fix.30069fd6.png"
+  },
+  {
+    "revision": "df6f2cce910339330ab229ba87010896",
+    "url": "/static/media/pp-main.df6f2cce.png"
+  }
+]);
