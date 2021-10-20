f8957f89105c6b8250b70b3c9963c2b992836aef
oseun <okseongeun4@gmail.com>
2021-10-20 16:06:38 +0900

    Updates

---
+++ precache-manifest.ca1eacb7da434d717d0e4518f861dbb4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce1724e4b7dfd877cce0fb4f2701e5d",
+    "url": "/MoPE-hub/index.html"
+  },
+  {
+    "revision": "dc1c63b8ac30cbe627ed",
+    "url": "/MoPE-hub/static/css/main.35ec5346.chunk.css"
+  },
+  {
+    "revision": "089a9e282ff9371b8562",
+    "url": "/MoPE-hub/static/js/2.b90b7b19.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/MoPE-hub/static/js/2.b90b7b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ccf7a4c9fe92082837b",
+    "url": "/MoPE-hub/static/js/3.d72ce1c0.chunk.js"
+  },
+  {
+    "revision": "dc1c63b8ac30cbe627ed",
+    "url": "/MoPE-hub/static/js/main.b7085edd.chunk.js"
+  },
+  {
+    "revision": "c30d5d73b8e2c4192c1e",
+    "url": "/MoPE-hub/static/js/runtime-main.228135da.js"
+  },
+  {
+    "revision": "0316627cd824392e0a79b58ad81b6f36",
+    "url": "/MoPE-hub/static/media/NotoSansCJKkr-Regular.0316627c.eot"
+  },
+  {
+    "revision": "81f8ae367221a20bc0b6c35477bd205f",
+    "url": "/MoPE-hub/static/media/NotoSansCJKkr-Regular.81f8ae36.woff"
+  },
+  {
+    "revision": "30069fd6dacc659f3d1e2f9df8f4e823",
+    "url": "/MoPE-hub/static/media/bg-fix.30069fd6.png"
+  },
+  {
+    "revision": "df6f2cce910339330ab229ba87010896",
+    "url": "/MoPE-hub/static/media/pp-main.df6f2cce.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd2d7d785cd563df3907cc23311ecbaf.js"
+  "/MoPE-hub/precache-manifest.ca1eacb7da434d717d0e4518f861dbb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MoPE-hub/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
